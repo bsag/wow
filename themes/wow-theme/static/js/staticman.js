3425d34ff1d1212ca103d95bda958e3ed56e33dc
bsag <butshesagirl@rousette.org.uk>
2018-03-25 13:05:13 +0100

    Made comment submission UI more user-friendly

--- themes/wow-theme/static/js/staticman.js
+++ themes/wow-theme/static/js/staticman.js
@@ -5,8 +5,11 @@
 
   $('.js-form').submit(function () {
     var form = this;
+    var std = $('#standard');
+    var loading = $('#loading');
 
-    $(form).addClass('form--loading');
+    $(std).removeClass('dib').addClass('dn');
+    $(loading).removeClass('dn').addClass('dib');
 
     $.ajax({
       type: $(this).attr('method'),
@@ -14,14 +17,16 @@
       data: $(this).serialize(),
       contentType: 'application/x-www-form-urlencoded',
       success: function (data) {
-        showModal('Perfect !', 'Thanks for your comment! It will show on the site once the site has been automatically rebuilt.');
-        $(form).removeClass('form--loading');
+        showModal('Perfect!', 'Thanks for your comment! It will show on the site once the site has been automatically rebuilt.');
+        $(std).removeClass('dn').addClass('dib');
+        $(loading).removeClass('dib').addClass('dn');
         $(form).trigger("reset");
       },
       error: function (err) {
         console.log(err);
         showModal('Error', 'Sorry, there was an error with the submission!');
-        $(form).removeClass('form--loading');
+        $(std).removeClass('dn').addClass('dib');
+        $(loading).removeClass('dib').addClass('dn');
       }
     });
 
@@ -29,13 +34,13 @@
   });
 
   $('.js-close-modal').click(function () {
-    $('body').removeClass('show-modal');
+    $('.modal').addClass('dn');
   });
 
   function showModal(title, message) {
     $('.js-modal-title').text(title);
     $('.js-modal-text').html(message);
 
-    $('body').addClass('show-modal');
+    $('.modal').removeClass('dn');
   }
 })(jQuery);
